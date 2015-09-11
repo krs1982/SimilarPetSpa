3a17d93e251337da512f1bd02c6a9661f3c24139
TytkoDominik <dominiktytko@gmail.com>
2015-09-11 23:47:28 +0200

    moving & destroying

--- Assets/AnimalBehaviour.cs
+++ Assets/AnimalBehaviour.cs
@@ -7,26 +7,28 @@ public class AnimalBehaviour : MonoBehaviour {
 	public Transform myNode;
 	[HideInInspector]
 	public int myNodeNumber = 0;
-
 	public GameObject Conveyor;
-
 	GameObject[] localNodes;
 
 	void Start()
 	{
+		Conveyor = GameObject.FindGameObjectWithTag ("Conveyor");
 		localNodes = Conveyor.GetComponent<ConveyorMoving>().nodes;
 		myNode = localNodes [myNodeNumber].transform;
 	}
 
 	public void MoveAnimal()
 	{
-		this.gameObject.GetComponent<TweenPosition> ().from = myNode.position;
-		myNodeNumber++;
-		myNode = localNodes[myNodeNumber].transform;
-		this.gameObject.GetComponent<TweenPosition> ().to = myNode.position;
-		this.gameObject.GetComponent<TweenPosition> ().Play (true);
-		this.gameObject.GetComponent<TweenPosition> ().Reset();
-
+		if (myNodeNumber == 12)
+			Destroy (this.gameObject);
+		else {
+			this.gameObject.GetComponent<TweenPosition> ().from = myNode.position;
+			myNodeNumber++;
+			myNode = localNodes [myNodeNumber].transform;
+			this.gameObject.GetComponent<TweenPosition> ().to = myNode.position;
+			this.gameObject.GetComponent<TweenPosition> ().Play (true);
+			this.gameObject.GetComponent<TweenPosition> ().Reset ();
+		}
 	}
 
 
