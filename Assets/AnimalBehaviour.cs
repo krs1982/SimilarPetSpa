4c3237393f9ccd5b3352cc880e63cd3077315534
TytkoDominik <dominiktytko@gmail.com>
2015-09-11 22:57:07 +0200

    NGUI added

---
+++ Assets/AnimalBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalBehaviour : MonoBehaviour {
+
+	[HideInInspector]
+	public Transform myNode;
+	[HideInInspector]
+	public int myNodeNumber = 0;
+
+	public GameObject Conveyor;
+
+	GameObject[] localNodes;
+
+	void Start()
+	{
+		localNodes = Conveyor.GetComponent<ConveyorMoving>().nodes;
+		myNode = localNodes [myNodeNumber].transform;
+	}
+
+	public void MoveAnimal()
+	{
+		this.gameObject.GetComponent<TweenPosition> ().from = myNode.position;
+		myNodeNumber++;
+		myNode = localNodes[myNodeNumber].transform;
+		this.gameObject.GetComponent<TweenPosition> ().to = myNode.position;
+		this.gameObject.GetComponent<TweenPosition> ().Play (true);
+		this.gameObject.GetComponent<TweenPosition> ().Reset();
+
+	}
+
+
+}
---
+++ Assets/ConveyorMoving.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ConveyorMoving : MonoBehaviour {
+
+	public GameObject[] nodes;
+	public GameObject animalsParent;
+	
+
+	public void MoveConveyor()
+	{
+		foreach (Transform child in animalsParent.transform)
+		{
+			child.gameObject.GetComponent<AnimalBehaviour>().MoveAnimal();
+		}
+
+	}
+}
