a9807f1063153ca4fc31339e2365626ae2fcf83f
krs1982 <lukaszkundys@gmail.com>
2015-09-11 23:55:51 +0200

    Commit
    Dodany GameManager do zarzadzania czasem przesuwania zwierzat, dodano
    naliczanie kombo z pokretla klawiatury midi

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    #region REFERENCES
+
+    private ConveyorMoving animalMover;
+    
+    #endregion REFERENCES
+
+    //... licznik do przesuwania zwierzat na tasmie
+    private float timer = 0;
+    
+    //... wskaznik naladowania combo sterowany pokretlem klawiatury MIDI
+    private int comboMeter = 0;
+
+    void Awake()
+    {
+        animalMover = GameObject.Find("Conveyor").GetComponent<ConveyorMoving>();	
+    }
+
+	void Start () 
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+	}
+	
+	void Update () 
+    {
+        timer += Time.deltaTime;
+	
+        if(timer >= 2f)
+        {
+            timer = 0;
+            animalMover.MoveConveyor();
+        }
+	}
+
+    public void IncreaseCombo()
+    {
+        comboMeter++;
+        Debug.Log(comboMeter.ToString());
+    }
+
+    private void ResetCombo()
+    {
+        comboMeter = 0;
+    }
+}
--- Assets/Scripts/MidiReceiver.cs
+++ Assets/Scripts/MidiReceiver.cs
@@ -10,6 +10,9 @@ public class MidiReceiver : MonoBehaviour{
 
     private InputDevice midiIn = null;
 
+    //... zmienne do systemu nabijania combosow
+    private bool up = true;
+
     void Start ()
     {
         midiIn = new InputDevice(0);
@@ -20,17 +23,46 @@ public class MidiReceiver : MonoBehaviour{
 
     private void HandleChannelMessageReceived(object sender, ChannelMessageEventArgs e)
     {
-        int message = e.Message.Data1;
+        int data1 = e.Message.Data1;
+        int data2 = e.Message.Data2;
 
-        switch(message)
+        //Debug.Log(data1.ToString() + ", " + data2.ToString());
+
+        if(data1 == 1)
         {
-            case 60: Debug.Log("Maszyna 01"); break;
-            case 62: Debug.Log("Maszyna 02"); break;
-            case 64: Debug.Log("Maszyna 03"); break;
-            case 65: Debug.Log("Maszyna 04"); break;
-            case 67: Debug.Log("Maszyna 05"); break;
+            if(up)
+            {
+                if(data2 > 90)
+                {
+                    up = false;
+                    GameManager.Instance.IncreaseCombo();
+                }
+            }
+            else if(!up)
+            {
+                if(data2 < 30)
+                {
+                    up = true;
+                    GameManager.Instance.IncreaseCombo();
+                }   
+            }
         }
+        else
+        {
+            switch (data1)
+            {
+                case 60: Debug.Log("Maszyna 01"); break;
+                case 62: Debug.Log("Maszyna 02"); break;
+                case 64: Debug.Log("Maszyna 03"); break;
+                case 65: Debug.Log("Maszyna 04"); break;
+                case 67: Debug.Log("Maszyna 05"); break;
+            }
+        }
+    }
 
+    void OnApplicationQuit()
+    {
+        midiIn.Close();
     }
 
 
