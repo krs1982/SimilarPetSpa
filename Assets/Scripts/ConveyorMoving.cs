43922770bc11f52e0584941c2a0c88e3053adf13
TytkoDominik <dominiktytko@gmail.com>
2015-09-12 12:37:39 +0200

    Spawner updated

--- Assets/Scripts/ConveyorMoving.cs
+++ Assets/Scripts/ConveyorMoving.cs
@@ -11,6 +11,8 @@ public class ConveyorMoving : MonoBehaviour {
 	public GameObject conveyorTop;
 	public GameObject conveyorBot;
 	public GameObject[] conveyorWheels;
+	int spawnerCounter = 0;
+	int spawnerHelper = 0;
 	
 
 	public void MoveConveyor()
@@ -25,6 +27,21 @@ public class ConveyorMoving : MonoBehaviour {
 		for (int c = 0; c < conveyorWheels.Length; c++) {
 			conveyorWheels[c].GetComponent<FactoryTapeRoller>().Roll ();
 		}
+
+		if (spawnerHelper == 0) {
+
+			animalsParent.GetComponent<AnimalsController> ().CreateRandomAnimal ();
+			spawnerCounter++;
+
+			if (spawnerCounter == 1)
+				spawnerHelper = 8;
+			else if (spawnerCounter < 3)
+				spawnerHelper = 3;
+			else 
+				spawnerHelper = Random.Range (2, 4);
+		}
+
+		spawnerHelper--;
 
 	}
 }
