29125ef7eff09c118c4a631f6c9c73f761614c55
TytkoDominik <dominiktytko@gmail.com>
2015-09-12 16:47:44 +0200

    AnimalBehaviour updated

--- Assets/Scripts/AnimalBehaviour.cs
+++ Assets/Scripts/AnimalBehaviour.cs
@@ -11,19 +11,26 @@ public class AnimalBehaviour : MonoBehaviour {
 	public int myNodeNumber = 0;
 	public GameObject Conveyor;
 	GameObject[] localNodes;
+	public Sprite startingSprite;
+	public Sprite progresSprite;
+	public Sprite happySprite;
+	SpriteRenderer animalSprite;
 
     public enum TREATMENTS { saw, magnet, tesla, syringe, roll};
     public List<TREATMENTS> neccesaryTreatments;
-
+	TREATMENTS nextTreatment;
 	void Start()
 	{
+		nextTreatment = neccesaryTreatments [0];
+		animalSprite = this.GetComponent<SpriteRenderer> ();
+		animalSprite.sprite = startingSprite;
 		Conveyor = GameObject.FindGameObjectWithTag ("Conveyor");
 		localNodes = Conveyor.GetComponent<ConveyorMoving>().nodes;
 		myNode = localNodes [myNodeNumber].transform;
         myNode.gameObject.GetComponent<NodeController>().AssignAnimalToNode(this.gameObject);
 		this.GetComponent<TweenPosition> ().duration = Conveyor.GetComponent<ConveyorMoving>().animationTime;
 		this.GetComponent<TweenPosition> ().to = myNode.transform.position;
-		this.GetComponent<TweenPosition> ().from = new Vector3 (-9.7f, -3.71f, 0);
+		this.GetComponent<TweenPosition> ().from = new Vector3 (-10.83f, -3.37f, 0);
 		this.GetComponent<TweenPosition> ().Play (true);
 		this.GetComponent<TweenPosition> ().Reset ();
 	}
@@ -43,6 +50,16 @@ public class AnimalBehaviour : MonoBehaviour {
 			this.gameObject.GetComponent<TweenPosition> ().Reset ();
 		}
 	}
+	public void ChangeSprite()
+	{
+		if (neccesaryTreatments [0] == nextTreatment) {
+			if (neccesaryTreatments.Count == 2)
+				animalSprite.sprite = progresSprite;
+			else if (neccesaryTreatments.Count == 1)
+				animalSprite.sprite = happySprite;
+		}
 
+
+	}
 
 }
