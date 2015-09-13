36f72ec64dc938ee691f7f14adbed3a63342cd0d
krs1982 <lukaszkundys@gmail.com>
2015-09-13 11:20:18 +0200

    Sceny

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour {
+
+    public void Play()
+    {
+        Application.LoadLevel("TutorialScene");
+    }
+
+    public void Play1()
+    {
+        Application.LoadLevel("TutorialScene1");
+    }
+
+    public void Play2()
+    {
+        Application.LoadLevel("MainScene");
+    }
+
+    public void Highscores()
+    {
+        Application.LoadLevel("HighscoresScene");
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel("MainMenuScene");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
